c69f5cfbb364f06de9dd8bf312d01a8373820c91
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2017-09-05 17:19:00 +0200

    Add an api to start tasks

---
+++ Controller/Rest/ApiStartTasksController.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @author: Brahim Boukoufallah <brahim.boukoufallah@idci-consulting.fr>
+ */
+
+namespace IDCI\Bundle\TaskBundle\Controller\Rest;
+
+use IDCI\Bundle\TaskBundle\Entity\TaskConfiguration;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use FOS\RestBundle\Controller\Annotations\Post;
+
+/**
+ * TaskConfiguration API REST controller
+ */
+class ApiStartTasksController extends Controller
+{
+    /**
+     * [POST] /start-tasks/{taskConfigurationId}
+     *
+     * Start tasks from a configuration
+     * The HTTP method used here might be controversial, as we are not treating resources, but starting process
+     *
+     * @Post("/start-tasks/{taskConfigurationId}")
+     *
+     * @return Response
+     */
+    public function getActionsAction(Request $request, $taskConfigurationId)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            throw new BadRequestHttpException();
+        }
+
+        $processor = $this->get('idci_task.processor.rabbitmq');
+        $manager = $this->get('idci_task.manager.task_configuration');
+        $taskConfiguration = $manager->findOneById($taskConfigurationId);
+        $processor->start($taskConfiguration);
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_NO_CONTENT);
+
+        return $response;
+    }
+}
---
+++ Resources/public/js/editor/src/workflow/components/action-configuration.vue
@@ -0,0 +1,127 @@
+<template>
+
+    <div class="configuration-box parameters">
+        <button @click.prevent="remove" aria-label="Close" class="close">
+            <span aria-hidden="true">×</span>
+        </button>
+        <h5>Chosen service: <strong>{{ action.service }}</strong></h5>
+        <div class="collapsed-block">
+            <div class="form-group">
+                <label>name</label>
+                <input class="form-control" v-model="actionName" type="text"/>
+            </div>
+            <a role="button" data-toggle="collapse" :href="'#'+ id" class="collapsed">
+                Parameters
+                <span class="toggle">
+                    <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                    <i class="fa fa-minus-circle" aria-hidden="true"></i>
+                </span>
+            </a>
+            <div :id="id" class="panel-collapse collapse" role="tabpanel" aria-expanded="false" :aria-controls="id">
+                <parameter
+                 v-for="(parameterOption, parameterName) in parameters"
+                 :key="parameterName"
+                 :name="parameterName"
+                 :option="parameterOption"
+                 :value="action.parameters[parameterName]"
+                 :required-star="true"
+                 @change="updateParameter"
+                 ></parameter>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+import parameter from '../../common/components/parameter.vue';
+import { utils } from 'vue-editor-commons';
+import 'TaskBundle/common/styles/collapsed-block.css';
+import 'TaskBundle/common/styles/editor.css';
+
+export default {
+
+    props: ['index'],
+
+    data: function () {
+        return {
+            actionName: null
+        };
+    },
+
+    computed: {
+        id: function () {
+            return 'action_parameters' + utils.generateUniqueId();
+        },
+
+        action: function () {
+            let action = this.$store.getters.getActionConfiguration(this.index);
+            this.actionName = action.name;
+
+            return action;
+        },
+
+        parameters: function () {
+            return this.getActionParameters(this.action.service);
+        }
+    },
+
+    watch: {
+        actionName: {
+            handler: function (newActionName) {
+                let payload = {
+                    actionIndex: this.index,
+                    name: newActionName
+                }
+
+                this.$store.commit('updateActionName', payload);
+            }
+        }
+    },
+
+    components: {
+        'parameter': parameter
+    },
+
+    methods: {
+
+        /**
+         * Update parameter
+         *
+         * @param {Object} parameter - the parameter object.
+         */
+        updateParameter: function (parameter) {
+            let payload = {
+                actionIndex: this.index,
+                parameter: parameter
+            };
+
+            this.$store.commit('updateParameter', payload);
+        },
+
+        /**
+         * Get the parameters for an action
+         *
+         * @returns {boolean}
+         */
+        getActionParameters: function (actionService) {
+            let action = this.$store.getters.getAction(actionService);
+
+            if (null != action) {
+                return action.parameters;
+            }
+
+            return {};
+        },
+
+        /**
+         * Remove an action
+         */
+        remove: function () {
+            this.$emit('remove');
+        }
+    }
+};
+
+</script>
