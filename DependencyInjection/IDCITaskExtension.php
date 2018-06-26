5f9c2265df8781bb74d60cf1902dfcfa9b211588
Brahim BOUKOUFALLAH <boukoufallah.brahim@gmail.com>
2018-06-26 15:59:30 +0200

    Set parameters on config prepend compilation.

--- DependencyInjection/IDCITaskExtension.php
+++ DependencyInjection/IDCITaskExtension.php
@@ -48,6 +48,16 @@ class IDCITaskExtension extends Extension implements PrependExtensionInterface
             );
             $container->prependExtensionConfig('idci_asset_loader', $config);
         }
+
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('idci_task.extract_rules', $config['extract_rules']);
+        $container->setParameter('idci_task.actions', $config['actions']);
+        $container->setParameter('idci_task.task_configuration_class', $config['task_configuration_class']);
+
+        $container->setParameter('idci_task.configuration', $config);
     }
 
     /**
@@ -55,18 +65,8 @@ class IDCITaskExtension extends Extension implements PrependExtensionInterface
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        $container->setParameter('idci_task.extract_rules', $config['extract_rules']);
-        $container->setParameter('idci_task.actions', $config['actions']);
-        $container->setParameter('idci_task.task_configuration_class', $config['task_configuration_class']);
-
-        $container->setParameter('idci_task.configuration', $config);
-
         $loader->load('constraints.yml');
     }
 }
